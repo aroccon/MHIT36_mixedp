1cadcc72f06eb765e525daa9d2c66234aa3c5e5e
Alessio Roccon <alessio.roccon@uniud.it>
2025-07-01 12:52:48 +0200

    typo_make_lib

--- make_lib_leo.sh
+++ make_lib_leo.sh
@@ -2,7 +2,7 @@ git clone https://github.com/NVIDIA/cuDecomp
 cd cuDecomp
 mkdir build
 cd build
-module laod profile/candidate
+module load profile/candidate
 module load nvhpc/25.3
 module load hpcx-mpi/2.19
 cmake ..
